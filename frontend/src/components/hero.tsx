fd1fc5dc056f7bcaf54b40b7d9e89903d315b0a7
Ubuntu <ubuntu@ip-172-31-64-139.ec2.internal>
2026-04-15 16:16:48 +0000

    update hero file

--- frontend/src/components/hero.tsx
+++ frontend/src/components/hero.tsx
@@ -1,7 +1,7 @@
 function hero() {
   return (
     <div className="mb-8 flex max-w-3xl flex-1 flex-col justify-end text-slate-50">
-      <h1 className="cursor-text text-xl font-bold md:text-4xl">Journey Beyond Horizons</h1>
+      <h1 className="cursor-text text-xl font-bold md:text-4xl">Journey Beyond Horizons updated in ECR Image</h1>
       <p className="my-4 cursor-text text-base md:text-lg">
         Dive into the world of travel with stories that transport you to far-off lands. Adventure
         awaits around every corner. It's time to explore the world!
